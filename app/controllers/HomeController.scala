9e7f6038a5968cc77b8b9947555dd27cfa0548a6
sato <nikona.s2u.5418@gmail.com>
2019-02-15 17:47:46 +0900

    user login

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,15 +2,20 @@ package controllers
 
 import javax.inject._
 
+import jp.t2v.lab.play2.auth.OptionalAuthElement
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+import services.UserService
 
 @Singleton
-class HomeController @Inject()(components: ControllerComponents)
+class HomeController @Inject()(val userService: UserService, components: ControllerComponents)
     extends AbstractController(components)
-      with I18nSupport {
+      with I18nSupport
+      with AuthConfigSupport
+      with OptionalAuthElement {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index: Action[AnyContent] = StackAction { implicit request =>
+    Ok(views.html.index(loggedIn))
   }
+
 }
